719a7cadd494528bdd01e41f4f615026a9b4b074
arve0 <arve.seljebu@gmail.com>
2015-05-14 16:06:43 +0200

    refactor to object oriented approach

--- microscopestitching/stitching.py
+++ microscopestitching/stitching.py
@@ -13,23 +13,24 @@ except NotImplementedError:
     _CPUS = 4
 
 
-def get_offset(images):
-    """Get translation to images.
+def calc_translations_parallel(images):
+    """Calculate image translations in parallel.
 
     Parameters
     ----------
-    images : list of tuple (path, row, column)
-        Each image-tuple should contain path, row and column.
-
-        Example:
-        >>> images = [('1.png', 0, 0), ('2.png', 0, 1)]
+    images : ImageCollection
+        Images as instance of ImageCollection.
 
     Returns
     -------
-    tuple, (ty, tx)
-        ty and tx is translation when moving in respectively x or y direction.
+    2d array, (ty, tx)
+        ty and tx is translation to previous image in respectively
+        x or y direction.
     """
-    pass
+    w = Parallel(n_jobs=_CPUS)
+    res = w(delayed(images.translation)(img) for img in images)
+    return np.array(res)
+
 
 def stitch(images):
     """Stitch regular spaced images.
@@ -49,18 +50,9 @@ def stitch(images):
         Merged image.
     """
     ic = ImageCollection(images)
-
-    # calculate translations
-    translations = []
-    for dim in (0, 1):
-        w = Parallel(n_jobs=_CPUS)
-        res = w(delayed(get_translation)(dim, i, ic) for i in ic.images)
-        res = _remove_none(res)
-        translations.append(res)
-
-    # for slice notation
-    y_translations = np.array(translations[0])
-    x_translations = np.array(translations[1])
+    translations = calc_translations_parallel(ic)
+    y_translations = translations[:,0]
+    x_translations = translations[:,1]
 
     # check that they are regular spaced
     xoffset = np.median(y_translations[:, 1])
@@ -81,73 +73,25 @@ def stitch(images):
 
     if xoffset != yoffset:
         print('Warning: yoffset != xoffset: %s != %s' % (yoffset, xoffset))
-        print("Assuming yoffset %s" % xoffset)
-        yoffset = xoffset
 
     # assume all images have the same shape
     img1 = imread(ic.images[0].path)
     y, x = img1.shape
-    height = y*ic.number_of_rows + yoffset*(ic.number_of_rows-1)
-    width = x*ic.number_of_cols + xoffset*(ic.number_of_cols-1)
+    height = y*len(ic.rows) + yoffset*(len(ic.rows)-1)
+    width = x*len(ic.cols) + xoffset*(len(ic.cols)-1)
 
     # last dimension is number of images on top of each other
     merged = np.zeros((height, width, 2), dtype=np.int)
     for r, c in product(ic.rows, ic.cols):
         mask = merge_slice(r, c, y, x, yoffset, xoffset)
         # last dim is used for averaging the seam
-        img = _add_ones_dim(imread(ic.image(r, c)))
+        img = _add_ones_dim(imread(ic.image(r, c).path))
         merged[mask] += img
 
     # average seam, possible improvement: use gradient
     merged[..., 0] /= merged[..., 1]
 
     return merged[..., 0].astype(np.uint8)
-
-
-def get_translation(dim, img, ic):
-    """
-    Parameters
-    ----------
-    dim : int
-        Which dimension to look for next image. 0 will look for image
-        below given row, col, anything else will look to the right for
-        ic.image(row, col).
-    img : Image
-        Image object with properties path, row and col.
-    ic : ImageCollection
-        Collection which hold neighbor images.
-
-    Returns
-    -------
-    tuple
-        (y_translation, x_translation, row, col)
-    """
-    row = img.row
-    col = img.col
-
-    if dim == 0:
-        # img1 on top of img2
-        next_row = row+1
-        next_col = col
-    else:
-        # img1 to the left of img2
-        next_row = row
-        next_col = col+1
-
-    img1_path = ic.image(row, col)
-    img2_path = ic.image(next_row, next_col)
-
-    if not img2_path:
-        # at the end
-        return
-
-    img1 = imread(img1_path)
-    img2 = imread(img2_path)
-
-    #tr, _, __ = register_translation(img1, img2, upsample_factor=100)
-    tr, _ = imreg.translation(img1, img2)
-    return tr[0], tr[1], row, col
-
 
 
 def merge_slice(row, col, height, width, yoffset, xoffset):
@@ -158,10 +102,20 @@ def merge_slice(row, col, height, width, yoffset, xoffset):
 
 
 class Image:
-    def __init__(self, path, row, col):
+    def __init__(self, path, row=None, col=None):
         self.path = path
         self.row = row
         self.col = col
+        self.translation = None
+
+
+    def __repr__(self):
+        return 'Image("%s", row=%s, col=%s)' % (self.path, self.row, self.col)
+
+
+    def __bool__(self):
+        return self.path != ''
+    __nonzero__=__bool__
 
 
 
@@ -172,8 +126,34 @@ class ImageCollection:
 
 
     def image(self, row, col):
-        return next((img[0] for img in self.image_list
-                     if img[1] == row and img[2] == col), '')
+        return next((img for img in self.images
+                     if img.row == row and img.col == col), Image(''))
+
+    def translation(self, img):
+        if type(img.translation) == np.ndarray:
+            return img.translation
+        else:
+            # img on top
+            top_img = self.image(img.row-1, img.col)
+            # img to the left
+            left_img = self.image(img.row, img.col-1)
+
+            if top_img:
+                img1 = imread(top_img.path)
+                img2 = imread(img.path)
+                y_translation, _ = imreg.translation(img1, img2)
+            else:
+                y_translation = (0, 0)
+
+            if left_img:
+                img1 = imread(left_img.path)
+                img2 = imread(img.path)
+                x_translation, _ = imreg.translation(img1, img2)
+            else:
+                x_translation = (0, 0)
+
+            img.translation = np.array((y_translation, x_translation))
+            return img.translation
 
 
     @property
@@ -186,14 +166,18 @@ class ImageCollection:
         return sorted(set([i[2] for i in self.image_list]))
 
 
-    @property
-    def number_of_rows(self):
-        return len(set([i[1] for i in self.image_list]))
+    def __iter__(self):
+        for img in self.images:
+            yield img
 
 
-    @property
-    def number_of_cols(self):
-        return len(set([i[2] for i in self.image_list]))
+    def __getitem__(self, index):
+        return self.images[index]
+
+
+    def __repr__(self):
+        return 'ImageCollection(\n  ' + ',\n  '.join([str(i) for i in self]) + ')'
+
 
 
 def _add_ones_dim(arr):
@@ -203,5 +187,19 @@ def _add_ones_dim(arr):
 
 
 def _remove_none(list_):
-    "Remove None objects from list."
+    "Remove None objects from list. Returns the new list."
     return [o for o in list_ if o != None]
+
+
+def _smooth_overlap(img):
+    "Create smooth overlap between images."
+    # img = np.ones((100,100))
+    #
+    # img[:10,:]  *= np.linspace(0, 1, 10)[:,np.newaxis]
+    # img[:,:10]  *= np.linspace(0, 1, 10)[np.newaxis,:]
+    #
+    # img[-10:,:] *= np.linspace(1, 0, 10)[:,np.newaxis]
+    # img[:,-10:] *= np.linspace(1, 0, 10)[np.newaxis,:]
+    #
+    # imshow(img)
+    pass
