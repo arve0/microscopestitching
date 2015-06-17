2550f5ddd2a1028dfdb664def2f1816f68205ebd
arve0 <arve.seljebu@gmail.com>
2015-06-17 19:36:44 +0200

    add test

--- test/test_something.py
+++ test/test_something.py
@@ -5,5 +5,14 @@ import pytest
 from microscopestitching import *
 
 
-def test_something(tmpdir):
-    pass
+def test_stitch():
+    "It should stitch side by side images."
+    images = [('test/0.png', 0, 0),
+              ('test/1.png', 0, 1)]
+
+    stitched, offset = stitch(images)
+
+    assert stitched.shape[1] > 512
+
+    assert offset[0] == 0 # only one row
+    assert offset[1] < 0
